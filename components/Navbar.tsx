d09c8d24c19cd8d8188a858909825f63cd441ba1
Myo Thet Kyaw <73188846+FrankyKyaw@users.noreply.github.com>
2023-07-01 15:08:22 -0400

    initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,95 @@
+"use client";
+import { useState } from "react";
+import { RiMoonFill, RiSunLine } from "react-icons/ri";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
+import { useTheme } from "next-themes";
+import {Link as ScrollLink } from "react-scroll/modules";
+
+interface NavItem {
+  label: string;
+  page: string;
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+  {
+    label: "Home",
+    page: "home",
+  },
+  {
+    label: "About",
+    page: "about",
+  },
+  {
+    label: "Projects",
+    page: "projects",
+  },
+];
+
+export const Navbar = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  const [navbar, setNavBar] = useState(false);
+
+  return (
+    <header className="w-full mx-auto px-4 dark:bg-stone-900 fixed top-0 z-50 sm:px-20 dark:border-b dark:border-stone-600 bg-white shadow">
+      <div className="justify-between md:items-center md:flex">
+        <div>
+          <div className="flex items-center py-4 justify-between">
+            <div className="md:block">
+              <h2 className="text-2xl font-bold">Franky Kyaw</h2>
+            </div>
+            <div className="md:hidden">
+              <button onClick={() => setNavBar(!navbar)}>
+                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              {NAV_ITEMS.map((item, idx) => {
+                return (
+                  <ScrollLink
+                    key={idx}
+                    to={item.page}
+                    className={
+                      "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"
+                    }
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                    onClick={() => setNavBar(!navbar)}
+                  >
+                    {item.label}
+                  </ScrollLink>
+                )
+              })}
+              {currentTheme === "dark" ? (
+                <button
+                  onClick={() => setTheme("light")}
+                  className="bg-slate-100 p-2 rounded-xl"
+                >
+                  <RiSunLine size={25} color="black" />
+                </button>
+              ) : (
+                <button
+                  onClick={() => setTheme("dark")}
+                  className="bg-slate-100 p-2 rounded-xl"
+                >
+                  <RiMoonFill size={25} />
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
