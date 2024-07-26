12c4f7de054c24c5818bff7c909910297230b046
Myo Thet Kyaw <73188846+FrankyKyaw@users.noreply.github.com>
2024-07-26 19:27:43 -0400

    update date

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
     <footer className="mx-auto mt-16 max-w-3xl px-4 sm:px-6 md:max-w-5xl">
       <hr className="w-full h-0.5 mx-automt-8 bg-neutral-200"></hr>
       <div className="mx-auto p-4 flex flex-col text-center md:flex-row md:justify-between">
-        <div className="text-neutral-500 dark:text-neutral-100">@2023 Franky Kyaw</div>
+        <div className="text-neutral-500 dark:text-neutral-100">@2024 Franky Kyaw</div>
         <div className="flex flex-row justify-center items-center space-x-2 mb-2">
           <a href="https://github.com/FrankyKyaw" rel="noreferrer" target="_blank">
             <AiOutlineGithub
